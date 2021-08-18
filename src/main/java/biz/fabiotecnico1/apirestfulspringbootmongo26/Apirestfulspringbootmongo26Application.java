db8e5f4c846a66eac276864f3156bb71bb213bc4
Fabio Luis <ml@fabiotecnico1.biz>
2021-08-17 22:17:19 -0300

    first commit

---
+++ src/main/java/biz/fabiotecnico1/apirestfulspringbootmongo26/Apirestfulspringbootmongo26Application.java
@@ -0,0 +1,13 @@
+package biz.fabiotecnico1.apirestfulspringbootmongo26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirestfulspringbootmongo26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirestfulspringbootmongo26Application.class, args);
+	}
+
+}
---
+++ src/test/java/biz/fabiotecnico1/apirestfulspringbootmongo26/Apirestfulspringbootmongo26ApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.fabiotecnico1.apirestfulspringbootmongo26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirestfulspringbootmongo26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
