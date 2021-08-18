f5b6d8443b03232ef99bc10ab7c44240f35cc8be
Fabio Luis <ml@fabiotecnico1.biz>
2021-08-18 20:40:16 -0300

    Adding comments to posts

--- src/main/java/biz/fabiotecnico1/apirestfulspringbootmongo26/domain/Post.java
+++ src/main/java/biz/fabiotecnico1/apirestfulspringbootmongo26/domain/Post.java
@@ -1,12 +1,15 @@
 package biz.fabiotecnico1.apirestfulspringbootmongo26.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import biz.fabiotecnico1.apirestfulspringbootmongo26.dto.AuthorDTO;
+import biz.fabiotecnico1.apirestfulspringbootmongo26.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {		
 	}
@@ -68,6 +73,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
