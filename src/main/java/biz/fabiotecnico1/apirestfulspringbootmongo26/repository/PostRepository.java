4f01ebdf54808fb59a04a6be152d603f10d5a358
Fabio Luis <ml@fabiotecnico1.biz>
2021-08-18 22:49:37 -0300

    Multi-criteria query

--- src/main/java/biz/fabiotecnico1/apirestfulspringbootmongo26/repository/PostRepository.java
+++ src/main/java/biz/fabiotecnico1/apirestfulspringbootmongo26/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package biz.fabiotecnico1.apirestfulspringbootmongo26.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,6 +15,15 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	//Alternative query
 	@Query("{'title': { $regex: ?0, $options: 'i'}}")
 	List<Post> searchTitle(String text);
+	
 	//Query methods
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	//Multi-criteria query
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} },"
+			+ " { $or: [ {'title': { $regex: ?0, $options: 'i'}}"
+			+ ", {'body': { $regex: ?0, $options: 'i'}},"
+			+ " {'comments.text': { $regex: ?0, $options: 'i'}} ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+	
 }
--- src/main/java/biz/fabiotecnico1/apirestfulspringbootmongo26/resources/PostResource.java
+++ src/main/java/biz/fabiotecnico1/apirestfulspringbootmongo26/resources/PostResource.java
@@ -1,5 +1,6 @@
 package biz.fabiotecnico1.apirestfulspringbootmongo26.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,19 @@ public class PostResource {
 		// decoded URL
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);		
+	}
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullsearch(
+			@RequestParam(value="text", defaultValue = "") String text,
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate){
+		// decoded URL
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);		
 	}
 }
