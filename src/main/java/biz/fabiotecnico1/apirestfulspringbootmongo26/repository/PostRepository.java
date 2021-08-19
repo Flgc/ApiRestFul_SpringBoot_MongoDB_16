2314d006b2bd6ebd8ec6b3a853ef62d9fb8f4316
Fabio Luis <ml@fabiotecnico1.biz>
2021-08-18 21:55:58 -0300

    Simple query with @Query

--- src/main/java/biz/fabiotecnico1/apirestfulspringbootmongo26/repository/PostRepository.java
+++ src/main/java/biz/fabiotecnico1/apirestfulspringbootmongo26/repository/PostRepository.java
@@ -3,6 +3,7 @@ package biz.fabiotecnico1.apirestfulspringbootmongo26.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import biz.fabiotecnico1.apirestfulspringbootmongo26.domain.Post;
@@ -10,7 +11,9 @@ import biz.fabiotecnico1.apirestfulspringbootmongo26.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	
+	//Alternative query
+	@Query("{'title': { $regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
 	//Query methods
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
